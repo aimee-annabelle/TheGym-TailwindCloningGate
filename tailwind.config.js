b2bb159d1e3a6c7b6da29eef7d73ef120e1ac4fd
aimee-annabelle <inezannabelle@gmail.com>
2024-05-17 09:43:53 +0200

    done: header section unresponsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,29 @@ module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "4rem",
+        lg: "6rem",
+        xl: "8rem",
+        "2xl": "12rem",
+        "3xl": "16rem",
+        "4xl": "20rem",
+      },
+      margin: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "4rem",
+        lg: "6rem",
+        xl: "8rem",
+        "2xl": "12rem",
+        "3xl": "16rem",
+        "4xl": "20rem",
+      },
+    },
   },
   plugins: [],
 };
